f8688b01ebd00e963c5d6007b1762ab8364294a0
Wes John-Alder <wjohnald@gmail.com>
2012-03-23 17:57:53 -0400

    Initial commit of template repository

---
+++ app.js
@@ -0,0 +1,51 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , _ = require('underscore')
+  ;
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/_views');
+  app.set('view engine', '_');
+  app.set('view options', {layout: false});
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Setup underscore as the view engine
+app.register('._', {
+    compile: function(str, options) {
+        var template = _.template(str);
+
+        return function(locals) {
+            return template(locals);
+        };
+    }
+});
+
+// Route middleware
+var filters = [
+];
+
+
+app.get('/', filters, require('./routes/index').index);
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
